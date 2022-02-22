1d7bb56668e7c856764b1c75fae6ed978c98b833
ElbarryAmine <81116690+elbarryamine@users.noreply.github.com>
2022-02-22 19:38:47 +0100

    Delete out directory

--- out/src/schema/user/user.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserSchema = void 0;
-const graphql_1 = require("graphql");
-const user_args_1 = require("./user.args");
-exports.UserSchema = new graphql_1.GraphQLObjectType({
-    name: 'User',
-    fields: () => user_args_1.UserSchemaArgs,
-});
