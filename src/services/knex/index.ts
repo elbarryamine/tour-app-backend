bc9c2674b908c9155779a57134fcef94f38ac70c
elbarryamine <mslmislam5@gmail.com>
2022-02-22 17:17:44 +0100

    changes

--- src/services/knex/index.ts
+++ src/services/knex/index.ts
@@ -4,11 +4,11 @@ dotenv.config()
 const db = knex({
   client: 'mysql2',
   connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT || 3306,
+    host: process.env.PLANETSCALE_DB_HOST,
+    database: process.env.PLANETSCALE_DB_NAME,
+    user: process.env.PLANETSCALE_DB_USER,
+    password: process.env.PLANETSCALE_DB_PASSWORD,
+    port: 3306,
   },
 })
 
